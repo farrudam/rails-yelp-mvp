8335034e7f4873d6504eaf44a0b2f7f5f1ad53b8
Fabio <fabioarrudamagalhaes@gmail.com>
2021-10-22 21:18:49 -0300

    alter routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: 'restaurants#index'
+  root to: 'restaurants#index'
   resources :restaurants do
     # NESTING:
     # /restaurants/:restaurant_id/reviews
